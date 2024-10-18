604531ce2889dd5c297e37dfb8eea76175e56c80
lvchien <v.chienle@gmail.com>
2024-10-18 09:52:02 +0200

    generate mesh for tetrahedron

--- src/primitives.jl
+++ src/primitives.jl
@@ -1500,3 +1500,52 @@ function meshopenbook(angle, nbofpages, h)
     rm(fno)
     return m
 end
+
+
+
+"""
+    meshuniformtetrahedron(edgelength, h)
+
+Create a mesh of a uniform tetrahedron with edge length `egdelength`. The four vertices are: (1, 1, 1), (-1, -1, 1), (-1, 1, -1) and (1, -1, -1) (rescaled).
+
+The target edge size is `h`.
+"""
+function meshuniformtetrahedron(edgelength, h)
+    
+    fno = tempname() * ".msh"
+    gmsh.initialize()
+    gmsh.model.add("tetrhedron")
+
+    c = edgelength/2√2
+    gmsh.model.geo.addPoint(c, c, c, h, 1)
+    gmsh.model.geo.addPoint(-c, -c, c, h, 2)
+    gmsh.model.geo.addPoint(-c, c, -c, h, 3)
+    gmsh.model.geo.addPoint(c, -c, -c, h, 4)
+
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(1, 3, 2)
+    gmsh.model.geo.addLine(1, 4, 3)
+    gmsh.model.geo.addLine(2, 3, 4)
+    gmsh.model.geo.addLine(2, 4, 5)
+    gmsh.model.geo.addLine(3, 4, 6)
+
+    gmsh.model.geo.addCurveLoop([-1, -4, 2], 1)
+    gmsh.model.geo.addPlaneSurface([1], 1)
+    gmsh.model.geo.addCurveLoop([-2, -6, 3], 2)
+    gmsh.model.geo.addPlaneSurface([2], 2)
+    gmsh.model.geo.addCurveLoop([-3, 5, 1], 3)
+    gmsh.model.geo.addPlaneSurface([3], 3)
+    gmsh.model.geo.addCurveLoop([4, -5, 6], 4)
+    gmsh.model.geo.addPlaneSurface([4], 4)
+
+    gmsh.model.geo.synchronize()
+    gmsh.option.setNumber("Mesh.MshFileVersion",2)
+    gmsh.model.mesh.generate(2)
+    gmsh.write(fno)
+    gmsh.finalize()
+
+    m = CompScienceMeshes.read_gmsh_mesh(fno)
+    rm(fno)
+    return m
+end
+
